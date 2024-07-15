dafddcbe8bb5c044be93d8aa686fb5ace401c9ad
Jaler Sekar Maji <jalerse@gmail.com>
2024-07-15 17:08:42 +0700

    AbstractDemo
    * Abstract
    * Interface
    * Base class
    * Virtual
    * Override

---
+++ AbstrasctDemo/DemoLibrary/SqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoLibrary
+{
+    public class SqlDataAccess : DataAccess
+    {
+
+        public override void LoadData(string sql)
+        {
+            Console.WriteLine("Loading Microsoft SQL Data");
+        }
+
+        public override void SaveData(string sql)
+        {
+            Console.WriteLine("Saving data to Microsoft SQL Server");
+        }
+    }
+}
